753c65b0c860632b63b7c2ef3d86aa2e0dce95fd
Shelby Lee <slee337@uw.edu>
2022-09-28 10:09:25 -0600

    commit tree cf46aaa03f5e06a7fba365fa0bc401eb6ed41e5c

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,12 @@
 class Network < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+
+    has_many :shows
+
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
+
+
 end
